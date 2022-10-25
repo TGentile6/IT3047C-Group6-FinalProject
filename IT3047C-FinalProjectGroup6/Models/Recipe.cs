2c6f976fd60354fed622270b7c59bdb893b4a2a3
TGentile6 <91705356+TGentile6@users.noreply.github.com>
2022-10-25 11:50:19 -0400

    added Recipe model

---
+++ IT3047C-FinalProjectGroup6/Models/Recipe.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IT3047C_FinalProjectGroup6.Models
+{
+    public class Recipe
+    {
+
+        public int RecipeID { get; set; }
+
+        [Required(ErrorMessage = "Recipe must include a name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Recipe must include a description.")]
+        public string Description { get; set; }
+        
+        [Required(ErrorMessage = "Recipe must include ingredients.")]
+        public string Ingredients { get; set; }
+
+        [Required(ErrorMessage = "Recipe must include a time estimate.")]
+        public int TimeEstimate { get; set; }
+
+        [Required(ErrorMessage = "Recipe must include an author.")]
+        public string Author { get; set; }
+
+
+    }
+}
