69c5baa74ea66c033db6271571550bdbb309a7d0
TGentile6 <91705356+TGentile6@users.noreply.github.com>
2022-11-17 17:44:04 -0500

    Redid CSS

---
+++ IT3047C-FinalProjectGroup6/Migrations/20221117210526_fifth.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IT3047C_FinalProjectGroup6.Migrations
+{
+    public partial class fifth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "Description", "TimeEstimate" },
+                values: new object[] { "A bright and creamy tomato based pasta sauce served with Penne.", 30 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "id",
+                keyValue: 2,
+                columns: new[] { "Description", "Ingredients", "Name" },
+                values: new object[] { "An unbeatable combination of both jelly and peanut butter between two slices of bread.", "Peanut Butter, Jelly, Bread", "Peanut Butter and Jelly Sandwich" });
+
+            migrationBuilder.InsertData(
+                table: "Recipes",
+                columns: new[] { "id", "Author", "Description", "Image", "Ingredients", "Name", "TimeEstimate" },
+                values: new object[,]
+                {
+                    { 3, "Saif Bayarri", "Cincinnati Chili is a famous dish in southwestern Ohio , with it’s most popular brand being Skyline Chili. The restaurant gets its name from the city skyline of Cincinnati.", "https://houseofnasheats.com/wp-content/uploads/2022/02/Cincinnati-Chili-Recipe-9-1.jpg", "Ground Beef, Cheddar Cheese, Chocolate, Pasta", "Cincinnati Chili", 45 },
+                    { 4, "JP Kelcewski", "A quick on-the-go meal with all the nutrients you need to start your day.", "https://lexiscleankitchen.com/wp-content/uploads/2017/10/Breakfast-Burritos-Freezer-Friendly5.jpg", "Eggs, Bacon or Sausage, Shredded Cheese, Bell Peppers, Diced Potatoes, Tortillas.", "Breakfast Burrito", 20 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "Description", "TimeEstimate" },
+                values: new object[] { "A bright and creamy tomato based pasta sauce served with Penne", 45 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "id",
+                keyValue: 2,
+                columns: new[] { "Description", "Ingredients", "Name" },
+                values: new object[] { "An unbeatable combination with both jelly and peanut butter in between two slices of bread", "Peanut butter, Jelly, Bread", "Peanut butter and jelly sandwich" });
+        }
+    }
+}
--- IT3047C-FinalProjectGroup6/Models/RecipeContext.cs
+++ IT3047C-FinalProjectGroup6/Models/RecipeContext.cs
@@ -16,23 +16,43 @@ namespace IT3047C_FinalProjectGroup6.Models
             modelBuilder.Entity<Recipe>().HasData(
                 new Recipe
                 {
-                    id = 1, 
-                    Name = "Vodka Pasta", 
-                    Description = "A bright and creamy tomato based pasta sauce served with Penne",
+                    id = 1,
+                    Name = "Vodka Pasta",
+                    Description = "A bright and creamy tomato based pasta sauce served with Penne.",
                     Ingredients = "Tomato Sauce, Heavy Whipping Cream, Olive Oil, Garlic, Pancetta (optional), Vodka, Basil, Penne",
                     Author = "Tony Gentile",
-                    TimeEstimate = 45,
+                    TimeEstimate = 30,
                     Image = "https://www.cook2eatwell.com/wp-content/uploads/2018/05/Pasta-with-Vodka-Sauce-Image-1.jpg"
                 },
                 new Recipe
                 {
                     id = 2,
-                    Name = "Peanut butter and jelly sandwich",
-                    Description = "An unbeatable combination with both jelly and peanut butter in between two slices of bread",
-                    Ingredients = "Peanut butter, Jelly, Bread",
+                    Name = "Peanut Butter and Jelly Sandwich",
+                    Description = "An unbeatable combination of both jelly and peanut butter between two slices of bread.",
+                    Ingredients = "Peanut Butter, Jelly, Bread",
                     Author = "Karan Patel",
                     TimeEstimate = 5,
                     Image = "https://www.gannett-cdn.com/media/2021/05/02/USATODAY/usatsports/imageForEntry14-XDZ.jpg"
+                },
+                new Recipe
+                {
+                    id = 3,
+                    Name = "Cincinnati Chili",
+                    Description = "Cincinnati Chili is a famous dish in southwestern Ohio , with it’s most popular brand being Skyline Chili. The restaurant gets its name from the city skyline of Cincinnati.",
+                    Ingredients = "Ground Beef, Cheddar Cheese, Chocolate, Pasta",
+                    Author = "Saif Bayarri",
+                    TimeEstimate = 45,
+                    Image = "https://houseofnasheats.com/wp-content/uploads/2022/02/Cincinnati-Chili-Recipe-9-1.jpg"
+                },
+                new Recipe
+                {
+                    id = 4,
+                    Name = "Breakfast Burrito",
+                    Description = "A quick on-the-go meal with all the nutrients you need to start your day.",
+                    Ingredients = "Eggs, Bacon or Sausage, Shredded Cheese, Bell Peppers, Diced Potatoes, Tortillas.",
+                    Author = "JP Kelcewski",
+                    TimeEstimate = 20,
+                    Image = "https://lexiscleankitchen.com/wp-content/uploads/2017/10/Breakfast-Burritos-Freezer-Friendly5.jpg"
                 }
             );
         }
